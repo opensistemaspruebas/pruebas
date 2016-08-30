39d8717125eb29620236cab697ea62dce731cc48
Marta Oliver <moliver@opensistemas.com>
2016-08-30 09:36:45 +0200

    Elimino tema proveedores y añadp tema por defecto de BBVA

---
+++ wordpress/wp-content/themes/bbva/content-link.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Template for displaying posts in the Link Post Format
+ *
+ * Used on index and archive pages
+ *
+ * @link https://codex.wordpress.org/Post_Formats
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+?>
+
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<header class="entry-header">
+			<hgroup>
+				<h2 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+				<h3 class="entry-format"><?php _e( 'Link', 'twentyeleven' ); ?></h3>
+			</hgroup>
+
+			
+		</header><!-- .entry-header -->
+
+		<?php if ( is_search() ) : // Only display Excerpts for Search ?>
+		<div class="entry-summary">
+			<?php the_excerpt(); ?>
+		</div><!-- .entry-summary -->
+		<?php else : ?>
+		<div class="entry-content">
+			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentyeleven' ) ); ?>
+			<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
+		</div><!-- .entry-content -->
+		<?php endif; ?>
+
+		
+	</article><!-- #post-<?php the_ID(); ?> -->
