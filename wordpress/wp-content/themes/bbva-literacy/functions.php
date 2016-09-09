95ac9621bf8ce92775e3cbae4fa508d7d6747e0e
Marta Oliver <moliver@opensistemas.com>
2016-09-09 09:21:31 +0200

    Arreglo functions.php

--- wordpress/wp-content/themes/bbva-literacy/functions.php
+++ wordpress/wp-content/themes/bbva-literacy/functions.php
@@ -1,30 +1,32 @@
 <?php
 	
+
 // Registro del menú de WordPress
-	add_theme_support( 'nav-menus' );
-	if ( function_exists( 'register_nav_menus' ) )
+add_theme_support('nav-menus');
+if (function_exists('register_nav_menus')) {
 	register_nav_menus(
 		array(
 		  'main' => 'menu_header'
 		)
 	);
+}
+	
 
-//Registro de widget 
-	
-//Registro de sidebar
- 
-if(function_exists('register_sidebar'))
-	
-	register_sidebar( array(
-		'name'          => 'Main Sidebar',
-		'id'            => 'sidebar-0',
-        'description'   => __( 'Sidebar Principal, columna completa', '' ),
-		'class'         => '',
-		'before_widget' => '',
-		'after_widget'  => '',
-        'before_title'  => '',
-		'after_title'   => '',
-	) );
+// Registro de sidebar
+if (function_exists('register_sidebar')) {
+	register_sidebar(
+		array(
+			'name' => 'Main Sidebar',
+			'id' => 'sidebar-0',
+			'description' => __('Sidebar Principal, columna completa'),
+			'class' => '',
+			'before_widget' => '',
+			'after_widget' => '',
+			'before_title' => '',
+			'after_title' => '',
+		)
+	);
+}
 	
 
 // Devuelve todas las etiquetas de un post separadas por comas
--- wordpress/wp-content/themes/bbva-literacy/single.php
+++ wordpress/wp-content/themes/bbva-literacy/single.php
@@ -8,7 +8,7 @@ get_header(); ?>
     
         <!-- Metas para el buscador. -->
         <?php echo get_search_meta_for_post($post); ?>
-        <!-- ******************************************* -->
+        <!-- ****************************************** -->
 
     	<main id="mainContent" class="template_single" data-role="content">
             <div class="wrapperFix">
