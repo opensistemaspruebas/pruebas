a9771a81c4bd67a362cb61195d65c870e1d1393c
Marta Oliver <moliver@opensistemas.com>
2016-09-09 14:27:52 +0200

    Cambios para tipo publicación

--- wordpress/wp-content/plugins/os_report_type/os_report_type.php
+++ wordpress/wp-content/plugins/os_report_type/os_report_type.php
@@ -38,7 +38,7 @@ function report_post_type() {
   );
   // Set other options for Custom Post Type
   $args = array(
-    'label'               => __('reports', 'os_report_type'),
+    'label'               => __('Reports', 'os_report_type'),
     'description'         => __('Reports', 'os_report_type'),
     'labels'              => $labels,
     'supports'            => array('title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields', ),
@@ -58,8 +58,165 @@ function report_post_type() {
     'taxonomies'          => array('category', 'post_tag', 'country'),
   );
   // Registering your Custom Post Type
-  register_post_type('reports', $args );
+  register_post_type('report', $args );
 }
+
+
+
+function reports_fields_function() {
+  if (function_exists("register_field_group")) {
+    register_field_group(array (
+      'id' => 'acf_fuente',
+      'title' => 'Fuente',
+      'fields' => array (
+        array (
+          'key' => 'field_57d2a8d7d0fb8',
+          'label' => 'Nombre',
+          'name' => 'nombre',
+          'type' => 'text',
+          'instructions' => 'Banco Mundial',
+          'default_value' => '',
+          'placeholder' => '',
+          'prepend' => '',
+          'append' => '',
+          'formatting' => 'none',
+          'maxlength' => '',
+        ),
+        array (
+          'key' => 'field_57d2a2d0a5753',
+          'label' => 'URL',
+          'name' => 'url',
+          'type' => 'text',
+          'instructions' => 'http://www.example.com/',
+          'default_value' => '',
+          'placeholder' => '',
+          'prepend' => '',
+          'append' => '',
+          'formatting' => 'html',
+          'maxlength' => '',
+        ),
+        array (
+          'key' => 'field_57d2a6d4148c2',
+          'label' => 'Fecha de publicación',
+          'name' => 'fecha_de_publicacion',
+          'type' => 'date_picker',
+          'instructions' => 'DD/MM/YYYY',
+          'date_format' => 'yymmdd',
+          'display_format' => 'dd/mm/yy',
+          'first_day' => 1,
+        ),
+        array (
+          'key' => 'field_57d2a824895c0',
+          'label' => 'Tipo',
+          'name' => 'tipo',
+          'type' => 'select',
+          'choices' => array (
+            'Informe' => 'Informe',
+          ),
+          'default_value' => 'Informe',
+          'allow_null' => 0,
+          'multiple' => 0,
+        ),
+        array (
+          'key' => 'field_57d2a84d895c1',
+          'label' => 'Público objetivo',
+          'name' => 'publico_objetivo',
+          'type' => 'select',
+          'choices' => array (
+            'Expertos' => 'Expertos',
+          ),
+          'default_value' => 'Expertos',
+          'allow_null' => 0,
+          'multiple' => 0,
+        ),
+        array (
+          'key' => 'field_57d2a92cc44e1',
+          'label' => 'Número de páginas',
+          'name' => 'numero_de_paginas',
+          'type' => 'number',
+          'instructions' => '1234',
+          'default_value' => '',
+          'placeholder' => '',
+          'prepend' => '',
+          'append' => '',
+          'min' => '',
+          'max' => '',
+          'step' => '',
+        ),
+        array (
+          'key' => 'field_57d2a94531506',
+          'label' => 'Código JEL',
+          'name' => 'codigo_jel',
+          'type' => 'number',
+          'instructions' => '13423534356546',
+          'default_value' => '',
+          'placeholder' => '',
+          'prepend' => '',
+          'append' => '',
+          'min' => '',
+          'max' => '',
+          'step' => '',
+        ),
+        array (
+          'key' => 'field_57d2a96294b76',
+          'label' => 'Edición',
+          'name' => 'edicion',
+          'type' => 'text',
+          'instructions' => '3º',
+          'default_value' => '',
+          'placeholder' => '',
+          'prepend' => '',
+          'append' => '',
+          'formatting' => 'html',
+          'maxlength' => '',
+        ),
+        array (
+          'key' => 'field_57d2a97828bd7',
+          'label' => 'Editorial',
+          'name' => 'editorial',
+          'type' => 'text',
+          'instructions' => 'BBVA Colombia',
+          'default_value' => '',
+          'placeholder' => '',
+          'prepend' => '',
+          'append' => '',
+          'formatting' => 'none',
+          'maxlength' => '',
+        ),
+        array (
+          'key' => 'field_57d2a994ff817',
+          'label' => 'Logo',
+          'name' => 'logo',
+          'type' => 'image',
+          'instructions' => 'Logo de la fuente',
+          'save_format' => 'object',
+          'preview_size' => 'thumbnail',
+          'library' => 'all',
+        ),
+      ),
+      'location' => array (
+        array (
+          array (
+            'param' => 'post_type',
+            'operator' => '==',
+            'value' => 'report',
+            'order_no' => 0,
+            'group_no' => 0,
+          ),
+        ),
+      ),
+      'options' => array (
+        'position' => 'normal',
+        'layout' => 'default',
+        'hide_on_screen' => array (
+        ),
+      ),
+      'menu_order' => 0,
+    ));
+  }
+}
+add_action('acf/register_fields', 'my_register_fields_function');
+
 
 
 /* Hook into the 'init' action so that the function
