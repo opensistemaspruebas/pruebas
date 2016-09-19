6708a4b006ca2e5ed45344be79b85590ff8e82c5
Marta Oliver <moliver@opensistemas.com>
2016-09-19 12:06:57 +0200

    Añado generación de json al guardar post

--- wordpress/wp-content/plugins/os_filter_widget/os_filter_widget.php
+++ wordpress/wp-content/plugins/os_filter_widget/os_filter_widget.php
@@ -151,20 +151,21 @@ endif;
 
 class JSONPost {
 
-    private static function saveInfo($name,$info,$post=false){
+    private static function saveInfo($name, $info, $post = false) {
         $debug = false;
         $path = '/var/www/bbvaLiteracy/wp-content/_json/';
-        if($post){
-            $path .= '_publicaciones/';
+
+        if ($post) {
+            $path .= '_' . get_post_type($name) . '/';
         }
         if (!is_dir($path)) {
-            mkdir($path,0777,true);
+            mkdir($path, 0777, true);
         }
-        $path .= $name.'.json';
-        if($debug){
-            echo "Guardamos ".$name."\r\n";
+        $path .= $name . '.json';
+        if ($debug) {
+            echo "Guardamos " . $name . "\r\n";
         }
-        if(!file_put_contents($path,json_encode($info))) {
+        if (!file_put_contents($path,json_encode($info))) {
             echo 'Error generando JSON del artículo';
             exit();
         }
@@ -177,47 +178,18 @@ class JSONPost {
 
     public static function savePost($post_id){
 
-    	//error_log("+++ paso por aqui +++");
-    	//error_log($post_id);
+		$args = array(
+		    'post_type' => array('publicacion', 'historia', 'taller'),
+		    'p' => $post_id
+		);
 
-        $os_query = new WP_Query('p=' . $post_id . '&post_type=publicacion');
+        $os_query = new WP_Query($args);
 
         $res = array();
         while ($os_query->have_posts()) : $os_query->the_post();
-
-        	error_log("bucle");
-
-            /*$content_partial = 'content';
-            if(isset($_POST['layout_type'])){
-                if($_POST['layout_type'] == 'v2'){
-                    $content_partial = 'v2-content';
-                }
-                if($_POST['layout_type'] == 'v3'){
-                    $content_partial = 'v3-content';
-                }
-                if($_POST['layout_type'] == 'v3-simple'){
-                    $content_partial = 'v3-content';
-                    global $forse_hide_element_read_more;
-                    global $forse_hide_element_date;
-                    $forse_hide_element_read_more = true;
-                    $forse_hide_element_date = true;
-                }
-                if(in_array($_POST['template_type'], array('page-masonry-condensed-facebook', 'page-masonry-simple-facebook'))){
-                    global $facebook_likes;
-                    $facebook_likes = true;
-                }
-                if($_POST['template_type'] == 'page-masonry-condensed-fixed-height'){
-                    global $forse_fixed_height;
-                    $forse_fixed_height = true;
-                }
-                if($_POST['template_type'] == 'page-masonry-condensed-with-author'){
-                    global $show_author_face;
-                    $show_author_face = true;
-                }
-            }
-            $res['body'] = os_load_template_part( $content_partial, get_post_format() );*/
-            $res['cats'] = wp_get_post_terms($post_id, array('category', 'ambito_geografico'));
-            JSONPost::saveInfo($post_id,$res,true);
+            $res['category'] = wp_get_post_terms($post_id, 'category', array("fields" => "ids"));
+            $res['ambito_geografico'] = wp_get_post_terms($post_id, 'ambito_geografico', array("fields" => "ids"));
+            JSONPost::saveInfo($post_id, $res, true);
         endwhile;
     }
 
