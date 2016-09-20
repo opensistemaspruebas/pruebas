95c36182cf5bbf1807938da3e771df4c5fd9d637
Marta Oliver <moliver@opensistemas.com>
2016-09-20 12:54:09 +0200

    Cambios en slide

--- wordpress/wp-content/plugins/os_slider_widget/os_slider_widget.php
+++ wordpress/wp-content/plugins/os_slider_widget/os_slider_widget.php
@@ -22,8 +22,6 @@ if (!class_exists('slider_widget')) :
                 'description' => __('Bluild Menu Slider with Introductions, Events, Testimonies and Pages posts','os_slider_widget')
             );
             $this->WP_Widget('slider', __('OS Slider Widget', 'os_slider_widget'), $options);
-            add_action('admin_enqueue_scripts', array(&$this, 'add_script_back'));
-            add_action('wp_enqueue_scripts', array(&$this, 'add_script_front'));
             add_action('admin_init', array(&$this, 'add_styles'));
         }
 
@@ -31,18 +29,6 @@ if (!class_exists('slider_widget')) :
 
             // Valores predeterminados para el widget
             $defaults = array(
-              'imagen1' => '', 
-              'imagen2' => '',
-              'imagen3' => '',
-            
-              'id_postp1' => '0', 
-              'id_postp2' => '0', 
-              'id_postp3' => '0',
-              
-              'nombre_menu1' => '', 
-              'nombre_menu2' => '', 
-              'nombre_menu3' => '',
-              
               'title1' => '', 
               'title2' => '', 
               'title3' => '',
@@ -53,20 +39,16 @@ if (!class_exists('slider_widget')) :
               
               'tlink1' => '',
               'tlink2' => '',
-              'tlink3' => ''
+              'tlink3' => '',
+
+              'ulink1' => '',
+              'ulink2' => '',
+              'ulink3' => ''
             );
                 
             $instance = wp_parse_args((array)$instance, $defaults);
             
             // Valores que se han introducido para el widget
-            $imagen1 = $instance['imagen1'];
-            $imagen2 = $instance['imagen2'];
-            $imagen3 = $instance['imagen3'];
-            
-            $id_postp1 = $instance['id_postp1'];
-            $id_postp2 = $instance['id_postp2'];
-            $id_postp3 = $instance['id_postp3'];
-            
             $nombre_menu1 = $instance['nombre_menu1'];
             $nombre_menu2 = $instance['nombre_menu2'];
             $nombre_menu3 = $instance['nombre_menu3'];
@@ -82,30 +64,10 @@ if (!class_exists('slider_widget')) :
             $tlink1 = $instance['tlink1'];
             $tlink2 = $instance['tlink2'];
             $tlink3 = $instance['tlink3'];
-            
-            
-            // Obtener páginas disponibles para links
-            $pages = get_pages(); 
-            foreach ($pages as $page) {
-                $output_pages1 .= '<option value="' . $page->ID . '"';
-                $output_pages2 .= '<option value="' . $page->ID . '"';
-                $output_pages3 .= '<option value="' . $page->ID . '"';
-                if ($page->ID == $id_postp1) {
-                    $output_pages1 .= 'selected';
-                }
-                if ($page->ID == $id_postp2) {
-                    $output_pages2 .= 'selected';
-                }
-                if ($page->ID == $id_postp3) {
-                    $output_pages3 .= 'selected';
-                }
-                $output_pages1 .= '>' . $this->truncate($page->post_title, 30);
-                $output_pages2 .= '>' . $this->truncate($page->post_title, 30);
-                $output_pages3 .= '>' . $this->truncate($page->post_title, 30);
-                $output_pages1 .= '</option>';
-                $output_pages2 .= '</option>';
-                $output_pages3 .= '</option>';
-            }
+
+            $ulink1 = $instance['ulink1'];
+            $ulink2 = $instance['ulink2'];
+            $ulink2 = $instance['ulink3'];
                 
             // Inicio de html para el formulario del widget    
             ?>  
@@ -113,16 +75,6 @@ if (!class_exists('slider_widget')) :
             <?php for ($i = 1; $i <= 3; $i++): ?>
                 <div class="slider-tab<?php echo $i; ?>" <?php if ($i > 3) { echo 'style="display:none;"'; } ?> >
                     <div class="bloque-item"><span style=""><?php _e("Elemento"); echo ' ' . $i; ?></span></div>
-                    <div class="campo-slider"><span><?php _e("Nombre del menú"); ?></span></div>
-                    <div class="valor-campo-slider">
-                        <input class="widefat" type="text" name="<?php echo $this->get_field_name('nombre_menu'.$i);?>" value="<?php echo esc_attr(${"nombre_menu" . $i});?>"/>
-                    </div>
-                    <div class="campo-slider"><span><?php _e("Imagen de fondo"); ?></span></div>
-                    <div class="valor-campo-imagen-slider" >
-                        <input class="url_imagen<?php echo $i; ?>" type="text" size="26" name="<?php echo $this->get_field_name('imagen'.$i);?>" value="<?php echo esc_attr(${"imagen" . $i});?>" readonly/> 
-                        <input name="subir_imagen<?php echo $i; ?>" class="button" type="button" value="<?php _e("Seleccionar imagen"); ?>" style=''/> 
-                        <img src="<?php echo esc_attr(${"imagen" . $i}); ?>" style='max-height:30px;margin-left:10px;' > 
-                    </div>
                     <div class="campos_tipo4" id="<?php echo $i; ?>">
                         <div class="campo-slider"><span><?php _e("Título"); ?></span></div>
                         <div class="valor-campo-slider" >
@@ -130,17 +82,17 @@ if (!class_exists('slider_widget')) :
                         </div>
                         <div class="campo-slider"> <span><?php _e("Descripción"); ?></span></div>
                         <div class="valor-campo-slider" >
-                            <input class="widefat" type="text" name="<?php echo $this->get_field_name('descr'.$i);?>" value="<?php echo esc_attr(${"descr" . $i});?>"/>
+                            <textarea rows="4" cols="20" class="widefat" name="<?php echo $this->get_field_name('descr'.$i);?>"><?php echo esc_attr(${"descr" . $i});?></textarea>
                         </div>
                         <div class="campo-slider"> <span><?php _e("Texto del enlace"); ?></span></div>
                         <div class="valor-campo-slider" >
                             <input class="widefat" type="text" name="<?php echo $this->get_field_name('tlink'.$i);?>" value="<?php echo esc_attr(${"tlink" . $i});?>"/>
                         </div>
-                        <div class="campo-slider"> <span><?php _e("Página a enlazar"); ?></span></div>
+                        <div class="campo-slider"> <span><?php _e("URL a enlazar"); ?></span></div>
                         <div class="valor-campo-slider paginas" > 
-                            <select class="type_slider<?php echo $i; ?>" name="<?php echo $this->get_field_name('id_postp'.$i);?>" >
-                                <?php echo ${"output_pages" . $i}; ?>
-                            </select>
+                            <div class="valor-campo-slider" >
+                                <input class="widefat" type="url" name="<?php echo $this->get_field_name('ulink'.$i);?>" value="<?php echo esc_attr(${"ulink" . $i});?>"/>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -155,22 +107,6 @@ if (!class_exists('slider_widget')) :
         function update($new_instance, $old_instance) {
             
             $instance = $old_instance;
-
-            $instance['imagen1'] = strip_tags($new_instance['imagen1']);
-            $instance['imagen2'] = strip_tags($new_instance['imagen2']);
-            $instance['imagen3'] = strip_tags($new_instance['imagen3']);
-            
-            $instance['tipo1'] = strip_tags($new_instance['tipo1']);
-            $instance['tipo2'] = strip_tags($new_instance['tipo2']);
-            $instance['tipo3'] = strip_tags($new_instance['tipo3']);
-            
-            $instance['id_postp1'] = strip_tags($new_instance['id_postp1']);
-            $instance['id_postp2'] = strip_tags($new_instance['id_postp2']);
-            $instance['id_postp3'] = strip_tags($new_instance['id_postp3']);
-
-            $instance['nombre_menu1'] = strip_tags($new_instance['nombre_menu1']);
-            $instance['nombre_menu2'] = strip_tags($new_instance['nombre_menu2']);
-            $instance['nombre_menu3'] = strip_tags($new_instance['nombre_menu3']);
             
             $instance['title1'] = strip_tags($new_instance['title1']);
             $instance['title2'] = strip_tags($new_instance['title2']);
@@ -183,6 +119,10 @@ if (!class_exists('slider_widget')) :
             $instance['tlink1'] = strip_tags($new_instance['tlink1']);
             $instance['tlink2'] = strip_tags($new_instance['tlink2']);
             $instance['tlink3'] = strip_tags($new_instance['tlink3']);
+
+            $instance['ulink1'] = strip_tags($new_instance['ulink1']);
+            $instance['ulink2'] = strip_tags($new_instance['ulink2']);
+            $instance['ulink3'] = strip_tags($new_instance['ulink3']);
 
             return $instance;
         }
@@ -276,25 +216,6 @@ if (!class_exists('slider_widget')) :
             
         function add_styles() {
             wp_enqueue_style('style-slider-widget', plugins_url( 'css/slider_style.css' , __FILE__));
-        }
-            
-            
-        function add_script_back() {
-            global $typenow;
-            if ($typenow == $this->post_type) {
-                //carga la librería para importar imágenes 
-                wp_enqueue_media();
-                wp_register_script('os_slider_widget_back-js', plugins_url( 'js/slider_widget_back.js' , __FILE__ ), array('jquery'));
-                wp_enqueue_script('os_slider_widget_back-js');
-            }
-        } 
-            
-            
-        function add_script_front() {
-            if(is_active_widget( false, false, $this->id_base, true)) { // check if widget is used
-                wp_register_script('os_slider_widget_front-js', plugins_url( 'js/slider_widget_front.js' , __FILE__ ), array('jquery'));
-                wp_enqueue_script('os_slider_widget_front-js');
-            }
         }
 
             
