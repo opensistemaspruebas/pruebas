5a9444639c4191629b17c5e8bc8aac0f1b1e06c6
Marta Oliver <moliver@opensistemas.com>
2016-09-21 11:31:26 +0200

    Añado selección de colores en tipo de contenido de impacto

---
+++ wordpress/wp-content/plugins/os_impacto_type/js/os_impacto_type.js
@@ -0,0 +1,3 @@
+jQuery(document).ready(function($) {
+	$('select[name="color"]').simplecolorpicker({theme: 'glyphicons'});
+});
--- wordpress/wp-content/plugins/os_impacto_type/os_impacto_type.php
+++ wordpress/wp-content/plugins/os_impacto_type/os_impacto_type.php
@@ -48,9 +48,63 @@ function impacto_type() {
       'hierarchical'       => false,
       'menu_position'      => null,
       'menu_icon'           => 'dashicons-chart-pie',
-      'supports'           => array('title', 'editor', 'thumbnail'),
-      'taxonomies'         => array('category', 'ambito_geografico')
+      'supports'           => array('title'),
+      'taxonomies'         => array()
   );
   register_post_type('impacto', $args );
 }
 add_action('init', 'impacto_type', 0);
+
+
+function impacto_meta_boxes() {
+  add_meta_box('impacto_color', __('Color del impacto', 'os_impacto_type'), 'meta_box_color', 'impacto', 'normal', 'high');
+}
+add_action('add_meta_boxes', 'impacto_meta_boxes');
+
+
+function load_impacto_scripts() {
+
+  // Cargar scripts
+  wp_register_script('jquery_simplecolorpicker_js', get_template_directory_uri() . '/resources/js/jquery.simplecolorpicker.js', array('jquery'));
+  wp_enqueue_script('jquery_simplecolorpicker_js');
+  wp_register_script('os_impacto_type_js', plugins_url('js/os_impacto_type.js' , __FILE__), array('jquery', 'jquery_simplecolorpicker_js'));
+  wp_enqueue_script('os_impacto_type_js');
+
+  // Cargar styles
+  wp_register_style('jquery_simplecolorpicker_css', get_template_directory_uri() . '/resources/css/jquery.simplecolorpicker.css', false, '1.0.0' );
+  wp_enqueue_style('jquery_simplecolorpicker_css');
+
+}
+add_action('admin_enqueue_scripts', 'load_impacto_scripts');
+
+
+// Campo para seleccionar el color
+function meta_box_color($post) {
+
+  wp_nonce_field(basename(__FILE__), 'meta_box_color-nonce');
+
+  $color = get_post_meta($post->ID, 'color', true);
+
+  ?>
+  <div class="form-group">
+    <label class="col-sm-4 control-label" for="colorpicker-bootstrap3-form"><?php _e('Seleccione un color', 'os_impacto_type'); ?></label>
+    <div class="col-sm-6">
+      <select name="color" id="color" class="form-control">
+        <option value="#5bbeff"><?php _e('Azul claro', 'os_impacto_type'); ?></option>
+        <option value="#f8cd51"><?php _e('Amarillo', 'os_impacto_type'); ?></option>
+        <option value="#14afb0"><?php _e('Azul verdoso', 'os_impacto_type'); ?></option>
+      </select>
+    </div>
+  </div>
+  <?php
+}
+
+
+function meta_boxes_impacto_save($post_id) {
+
+  if (isset($_POST['color'])) {
+    update_post_meta($post_id, 'color', strip_tags($_POST['color']));
+  }
+
+}
+add_action('save_post', "meta_boxes_impacto_save");
