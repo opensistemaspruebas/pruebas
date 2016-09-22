6fef85e3990ddf96fc9ed27f93bc985255febd5a
Marta Oliver <moliver@opensistemas.com>
2016-09-22 17:24:53 +0200

    Sigo con Twitter

--- wordpress/wp-content/plugins/os_twitter_widget/js/os_twitter_widget.js
+++ wordpress/wp-content/plugins/os_twitter_widget/js/os_twitter_widget.js
@@ -10,6 +10,8 @@ jQuery(document).ready(function() {
 		console.log("he hecho la llamada a twitter");
 	
 		if (d.code === 200 && d.data.length > 0) {
+
+			jQuery('.row.tweets-container').html('');
 	        
 	        jQuery.each(d.data, function(i, result) {
 	        	console.log(result);
@@ -17,9 +19,19 @@ jQuery(document).ready(function() {
 	        	var nombre = result.user.name;
 	        	var usuario = '@' + result.user.screen_name;
 	        	var imagen = result.user.profile_image_url_https;
-	        	var likes = result.user.favourites_count;
-	        	var fecha = result.created_at;
+	        	var likes = result.favorite_count;
+	        	var fecha = new Date(result.created_at);
 	        	var texto = result.text;
+
+	        	var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+	        	fecha = fecha.getDate() + ' ' +  meses[fecha.getMonth()];
+	        	fecha = fecha.toString();
+	        	fecha = fecha.toLowerCase();
+	        	
+	        	texto = texto.replace(/#(\S*)/g,'<b>#$1</b>');
+	        	texto = texto.replace(/@(\S*)/g,'<b>@$1</b>');
+	        	texto = texto.replace(/http(\S*)/g,'<b>http$1</b>');
 
 	        	jQuery('.row.tweets-container').append('<div class="col-xs-10 col-xs-offset-1 col-sm-offset-0 col-sm-4"> <section class="row tweet"> <div class="image-container nopadding col-xs-3"> <img src="' + imagen + '" alt=""> </div> <div class="data-container col-xs-9"> <div class="user-info"> <span class="user-name">' + nombre + '</span> <span class="account">' + usuario + '</span> </div> <p class="tweet-content mt-xxs mb-xxs"> ' + texto + ' </p> <div class="row statistics"> <div class="col-xs-2 col-sm-2 nopadding likes"> <span class="bbva-icon-favorite"></span> <span>' + likes + '</span> </div> <div class="col-xs-1 col-sm-2 nopadding share"> <span class="bbva-icon-share"></span> </div> <div class="col-xs-9 col-sm-8 nopadding date"> <span>' + fecha + '</span> </div> </div> </div> </section> </div>');
 
