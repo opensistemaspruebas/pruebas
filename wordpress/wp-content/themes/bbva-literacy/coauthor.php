d3db6a23e03824fa7f8a9b950ac993401b6d61f7
Marta Oliver <moliver@opensistemas.com>
2016-10-13 16:26:37 +0200

    Pongo js en sitio bueno

--- wordpress/wp-content/themes/bbva-literacy/coauthor.php
+++ wordpress/wp-content/themes/bbva-literacy/coauthor.php
@@ -153,33 +153,6 @@
 
         ?>
         
-        <script>
-            jQuery(document).ready(function($) {
-                numero_trabajos_ocultos = jQuery('article#otros_trabajos .content .data-block:hidden').length;
-                if (numero_trabajos_ocultos == 0) {
-                    jQuery('a#readmore_trabajos').remove();
-                }
-                numero_trabajos_visibles = jQuery('article#otros_trabajos .content .data-block:visible').length;
-                jQuery('#readmore_trabajos').on('click', function(e) {
-                    e.preventDefault();
-                    numero_trabajos_ocultos = jQuery('article#otros_trabajos .content .data-block:hidden').length;
-                    i = 1;
-                    jQuery('article#otros_trabajos .content .data-block:hidden').each(function() {
-                        jQuery('article#otros_trabajos .content #trabajo_' + numero_trabajos_visibles).show();
-                        numero_trabajos_visibles++;
-                        if (i == 3) {
-                            return false;
-                        } else {
-                            i++;
-                        }
-                    });
-                    numero_trabajos_ocultos = jQuery('article#otros_trabajos .content .data-block:hidden').length;
-                    if (numero_trabajos_ocultos == 0) {
-                        jQuery('a#readmore_trabajos').remove();
-                    }
-                });
-            });
-        </script>
         <?php if (!empty($trabajos)) : ?>
             <article id="otros_trabajos" name="otros_trabajos" class="container data-grid">
                 <header>
--- wordpress/wp-content/themes/bbva-literacy/resources/js/script-os.js
+++ wordpress/wp-content/themes/bbva-literacy/resources/js/script-os.js
@@ -5,5 +5,29 @@ jQuery(document).ready(function($) {
 		var val = jQuery(this).find(":selected").val();
 		window.location.href = val;
 	});
+	
+	numero_trabajos_ocultos = jQuery('article#otros_trabajos .content .data-block:hidden').length;
+	if (numero_trabajos_ocultos == 0) {
+	    jQuery('a#readmore_trabajos').remove();
+	}
+	numero_trabajos_visibles = jQuery('article#otros_trabajos .content .data-block:visible').length;
+	jQuery('#readmore_trabajos').on('click', function(e) {
+	    e.preventDefault();
+	    numero_trabajos_ocultos = jQuery('article#otros_trabajos .content .data-block:hidden').length;
+	    i = 1;
+	    jQuery('article#otros_trabajos .content .data-block:hidden').each(function() {
+	        jQuery('article#otros_trabajos .content #trabajo_' + numero_trabajos_visibles).show();
+	        numero_trabajos_visibles++;
+	        if (i == 3) {
+	            return false;
+	        } else {
+	            i++;
+	        }
+	    });
+	    numero_trabajos_ocultos = jQuery('article#otros_trabajos .content .data-block:hidden').length;
+	    if (numero_trabajos_ocultos == 0) {
+	        jQuery('a#readmore_trabajos').remove();
+	    }
+	});
 	 
 });
