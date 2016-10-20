6bba8fac27046fb615a9ad1b7d641f287d1f009d
Marta Oliver <moliver@opensistemas.com>
2016-10-20 17:20:40 +0200

    Arreglo problema con perfiles repetidos

--- wordpress/wp-content/plugins/os_perfiles_de_usuario/os_perfiles_de_usuario.php
+++ wordpress/wp-content/plugins/os_perfiles_de_usuario/os_perfiles_de_usuario.php
@@ -486,11 +486,11 @@ function create_perfiles_taxonomy() {
     // Call the register_taxonomy function
     register_taxonomy($taxonomy, $object_type, $args); 
 
-    wp_insert_term(__('Miembro', 'os_perfiles_de_usuario'), 'perfil');
+    /*wp_insert_term(__('Miembro', 'os_perfiles_de_usuario'), 'perfil');
     wp_insert_term(__('Coordinador', 'os_perfiles_de_usuario'), 'perfil');
     wp_insert_term(__('Asesor', 'os_perfiles_de_usuario'), 'perfil');
     wp_insert_term(__('Autor', 'os_perfiles_de_usuario'), 'perfil');
-    wp_insert_term(__('Ponente', 'os_perfiles_de_usuario'), 'perfil');
+    wp_insert_term(__('Ponente', 'os_perfiles_de_usuario'), 'perfil');*/
 
 }
 add_action('init', 'create_perfiles_taxonomy', 0);
