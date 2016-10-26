6c5f83eaaa8bf99c0d9640c2d93c599d0b11c777
Roberto Moreno <rmoreno@opensistemas.com>
2016-10-26 14:44:41 +0200

    Añadida funcion para dar formato a las cantidades de impactos

--- wordpress/wp-content/themes/bbva-literacy/functions.php
+++ wordpress/wp-content/themes/bbva-literacy/functions.php
@@ -188,8 +188,11 @@ function thousandsCurrencyFormat($num) {
 	$x_count_parts = count($x_array) - 1;
 	$x_display = $x;
 	$x_display = $x_array[0] . ((int) $x_array[1][0] !== 0 ? ',' . $x_array[1][0] : '');
-	$x_display .= $x_parts[$x_count_parts - 1];
-	return $x_display;
+	$x_display2 = $x_parts[$x_count_parts - 1];
+
+	$values = array($x_display, $x_display2);
+
+	return $values;
 }
 
 
