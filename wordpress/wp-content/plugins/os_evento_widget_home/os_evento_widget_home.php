f355b6beae73b73fd755aaaeed2239fcdcd08ddf
Marta Oliver <moliver@opensistemas.com>
2016-11-07 17:32:41 +0100

    Evento widget home

--- wordpress/wp-content/plugins/os_evento_widget_home/os_evento_widget_home.php
+++ wordpress/wp-content/plugins/os_evento_widget_home/os_evento_widget_home.php
@@ -43,7 +43,7 @@ if (!class_exists('os_evento_widget_home')) :
 				'orderby' 		   => 'meta_value',
 				'post_type'        => 'evento',
 				'post_status'      => 'publish',
-				'suppress_filters' => true,
+				'suppress_filters' => false,
 			);
 
 	    	$evento = get_posts($args);
