7e751b0810ddd7d7fd674a15a5dc9eafbf0fa87e
Roberto Moreno <rmoreno@opensistemas.com>
2016-11-28 12:45:07 +0100

    Widget practicas completado

--- wordpress/wp-content/plugins/os_practicas_widget/os_practicas_widget.php
+++ wordpress/wp-content/plugins/os_practicas_widget/os_practicas_widget.php
@@ -30,10 +30,19 @@ if (!class_exists('OSPracticasWidget')) :
 
 
 	    public function widget($args, $instance) {
+
+	    	$titulo = $instance['titulo'];
+	    	$descripcion = $instance['descripcion'];
+	    	$titulo1 = $instance['titulo1'];
+	    	$titulo2 = $instance['titulo2'];
+	    	$titulo3 = $instance['titulo3'];
+	    	$pdf = $instance['pdf'];
+	    	$pdfInterno = $instance['pdfInterno'];
 	    	
 	    	$imagen_url1 = '';
 	    	$imagen_url2 = '';
 	    	$imagen_url3 = '';
+
 	    	
 	    	if($instance['imagen1'] != '') {
 	    		$imagen_url1 = wp_get_attachment_url($instance['imagen1']);
@@ -47,41 +56,66 @@ if (!class_exists('OSPracticasWidget')) :
     	?>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    		<header class="initial-header-image wow fadeIn">
-		      <span><?php echo $instance['titulo']; ?></span>
-		      <img class="img-responsive" src="<?php echo $imagen_url; ?>" alt="">
-		    </header>
+ 			<article id="bestPractices">
+                <div class="beginning">
+                    <div class="container">
+                    	<?php if(!empty($titulo) || !empty($descripcion)) : ?>
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-8">
+                                <h2><?php echo $titulo; ?></h2>
+                                <p><?php echo $descripcion; ?></p>
+                            </div>
+                        </div>
+                        <?php endif; ?>
+                        <?php if(!empty($titulo1) || !empty($titulo2) || !empty($titulo3)) : ?>
+                        <div class="section-practices row">
+                            <div class="col-xs-12 col-sm-4 text-center">
+                                <div class="content-img">
+                                    <img src="<?php echo $imagen_url1; ?>">
+                                </div>
+                                <span><?php echo $titulo1; ?></span>
+                            </div>
+                            <div class="col-xs-12 col-sm-4 text-center">
+                                <div class="content-img">
+                                    <img src="<?php echo $imagen_url2; ?>">
+                                </div>
+                                <span><?php echo $titulo2; ?></span>
+                            </div>
+                            <div class="col-xs-12 col-sm-4 text-center">
+                                <div class="content-img">
+                                    <img src="<?php echo $imagen_url3; ?>">
+                                </div>
+                                <span><?php echo $titulo3; ?></span>
+                            </div>
+                        </div>
+                        <?php endif; ?>
+                    </div>
+                </div>
+                <?php if(!empty($pdf) || !empty($pdfInterno)) : ?>
+                <section class="container">
+                    <div class="col-xs-12 download-file">
+                        <div class="row">
+                            <div class="pdf-rectangle">
+                                <div class="col-xs-12 col-sm-1">
+                                    <div class="content-icon">
+                                        <span class="icon bbva-icon-pdf-01"></span>
+                                    </div>
+                                </div>
+                                <div class="col-xs-12 col-sm-6 text-content">
+                                    <h2><?php _e("Principios", "os_practicas_widget"); ?></h2>
+                                    <p><?php _e("Consulta y/o descarga el PDF con los principios", "os_practicas_widget"); ?></p>
+                                </div>
+                                <div class="col-xs-12 col-sm-5">
+                                    <div class="container-button text-right">
+                                        <a target="_blank" <?php if(!empty($pdf)){ ?> href="<?php echo $pdf; ?>" <?php } else if(!empty($pdfInterno)){ ?> href="<?php echo $pdfInterno; ?>" <?php } ?> class="btn btn-bbva-aqua"><?php _e("Ver PDF", "os_practicas_widget"); ?></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <?php endif; ?>
+			</article>
 
 	    	<?php
 
@@ -90,6 +124,8 @@ if (!class_exists('OSPracticasWidget')) :
 
 	    public function form($instance) {
 	    	
+	    	$titulo = ! empty($instance['titulo']) ? $instance['titulo'] : '';
+	    	$descripcion = ! empty($instance['descripcion']) ? $instance['descripcion'] : '';
 	    	$titulo1 = ! empty($instance['titulo1']) ? $instance['titulo1'] : '';
 	    	$titulo2 = ! empty($instance['titulo2']) ? $instance['titulo2'] : '';
 	    	$titulo3 = ! empty($instance['titulo3']) ? $instance['titulo3'] : '';
@@ -109,11 +145,18 @@ if (!class_exists('OSPracticasWidget')) :
 	    		$imagen_url3 = wp_get_attachment_url($imagen3);
 	    	}
 
-	    	os_imprimir($instance, 0);
-
 	    	
 	    	?>
-
+	    	
+	    	<p>
+			    <label for="<?php echo $this->get_field_id('titulo'); ?>"><?php _e('Título común a los principios', 'os_practicas_widget'); ?></label>
+			    <input type="url" class="widefat" id="<?php echo $this->get_field_id('titulo'); ?>" name="<?php echo $this->get_field_name('titulo'); ?>" value="<?php echo $titulo; ?>"/>
+			</p>
+			<p>
+			    <label for="<?php echo $this->get_field_id('descripcion'); ?>"><?php _e('URL externa del archivo PDF', 'os_practicas_widget'); ?></label>
+			    <textarea type="text" style="width:100%;" rows="4" class="widefat" id="<?php echo $this->get_field_id('descripcion'); ?>" name="<?php echo $this->get_field_name('descripcion'); ?>" value=""><?php echo $descripcion; ?></textarea>
+  			</p>
+			</p>
 	    	<div style="border-width: 0px 0px 1px 0px;  border-color: #CAC6C6; border-style: solid;">
 				<p>
 					<label for="<?php echo $this->get_field_id('titulo1'); ?>"><?php _e('Título 1:', 'os_practicas_widget'); ?></label>
@@ -147,15 +190,15 @@ if (!class_exists('OSPracticasWidget')) :
 					<input id="upload_image3" class="button button-hero os_practica_widget-control3-choose" type="button" value="Selecciona imagen 3"/>
 				</p>
 			</div>
-			 <p >
+			<p>
 			    <label for="<?php echo $this->get_field_id('pdf'); ?>"><?php _e('URL externa del archivo PDF', 'os_practicas_widget'); ?></label>
 			    <input type="url" class="widefat" id="<?php echo $this->get_field_id('pdf'); ?>" name="<?php echo $this->get_field_name('pdf'); ?>" value="<?php echo $pdf; ?>"/>
-			 </p>
-			 <p class="os_practica_widget-controlPdfInterno" data-title="Selecciona PdfInterno " data-update-text="Selecciona PdfInterno" data-target=".PdfInterno">
+			</p>
+			<p class="os_practica_widget-controlPdfInterno" data-title="Selecciona PdfInterno " data-update-text="Selecciona PdfInterno" data-target=".PdfInterno">
 			    <label for="<?php echo $this->get_field_id('pdfInterno'); ?>"><?php _e('Cargador de archivo PDF', 'os_practicas_widget'); ?></label></br>
 			    <input class="PdfInterno os_practica_widget-controlPdfInterno-target widefat" type="url" id="<?php echo $this->get_field_id('pdfInterno'); ?>" name="<?php echo $this->get_field_name('pdfInterno'); ?>" value="<?php echo $pdfInterno; ?>" readonly></br>
 			 	<input id="upload_pdfInterno" class="button button-hero os_practica_widget-controlPdfInterno-choose" name="upload_pdfInterno" type="button" value="<?php _e('Explorar/Subir', 'os_publicacion_type'); ?>" />
-			 </p>
+			</p>
 					
 						
 			<?php
@@ -166,6 +209,8 @@ if (!class_exists('OSPracticasWidget')) :
 
 			$instance = $old_instance;
 
+			$instance['titulo'] = (!empty( $new_instance['titulo'])) ? strip_tags($new_instance['titulo']) : '';
+			$instance['descripcion'] = (!empty( $new_instance['descripcion'])) ? strip_tags($new_instance['descripcion']) : '';
 			$instance['titulo1'] = (!empty( $new_instance['titulo1'])) ? strip_tags($new_instance['titulo1']) : '';
 			$instance['titulo2'] = (!empty( $new_instance['titulo2'])) ? strip_tags($new_instance['titulo2']) : '';
 			$instance['titulo3'] = (!empty( $new_instance['titulo3'])) ? strip_tags($new_instance['titulo3']) : '';
